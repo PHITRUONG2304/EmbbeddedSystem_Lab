ef50198c7e60769a51fb0c0954204881d106ad57
PHITRUONG2304 <truong.truong.01.04.23@gmail.com>
2022-11-21 07:43:05 +0700

    Prioritized Pre-emptive Scheduling (without Time Slicing)

--- Lab3/main/main.c
+++ Lab3/main/main.c
@@ -6,14 +6,17 @@
 #include "freertos/task.h"
 #include "sdkconfig.h"
 
-TaskHandle_t idleTask;
-TaskHandle_t task2;
+TaskHandle_t  idleTask;
+TaskHandle_t  task2;
 
 void task1Function( void *pvParameters )
 {
 	while(1){
-		vTaskDelay(130);
 		printf("---------------------Task 1 is running!------------------\n");
+		vTaskDelay(10);
+
+		printf("---------------------Task 1 is running!------------------\n");
+		vTaskDelay(100);
 	}
 }
 
@@ -21,8 +24,6 @@ void task2Function( void *pvParameters )
 {
 	while(1){
 		printf("Task 2 is running!\n");
-		vTaskResume( idleTask );
-		vTaskSuspend( task2 );
 	}
 }
 
@@ -30,20 +31,15 @@ void idleTaskFunction( void *pvParameters )
 {
 	while(1){
 		printf("Idle task is running!\n");
-		vTaskResume( task2 );
-		vTaskSuspend( idleTask );
 	}
 }
 
 
 void app_main(void)
 {
-    xTaskCreate(task1Function, "Task 1", 4096, NULL, 2, NULL);
+    xTaskCreate(task2Function, "Task 2", 8192, NULL, 0, NULL);
+    xTaskCreate(idleTaskFunction, "Idle task", 8192, NULL, 0, NULL);
 
-    xTaskCreate(idleTaskFunction, "Idle task", 4096, NULL, 0, &idleTask);
-    xTaskCreate(task2Function, "Task 2", 4096, NULL, 0, &task2);
-
-    vTaskSuspend( task2 );
-
-//    vTaskStartScheduler();
+	vTaskDelay(100);
+    xTaskCreate(task1Function, "Task 1", 4096, NULL, 3, NULL);
 }
